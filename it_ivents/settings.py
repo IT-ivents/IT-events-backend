b7b1dda731135997498b291a4c84088138ceaab4
Barinov Maksim <craftyplonkton@gmail.com>
2023-05-24 22:27:55 +0300

    remove secret key

--- it_ivents/settings.py
+++ it_ivents/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-atnz(v^839jkr*njyzten8xnfnxb24o@$8-8-*@$f5zbyeni#8'
+SECRET_KEY = 'not_a_secret_at_all'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
