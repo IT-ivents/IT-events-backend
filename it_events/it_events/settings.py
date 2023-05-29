78bf8e2cdadd97c1841fd3ad32607df1f08bd563
Asterrus <gastursolar@gmail.com>
2023-05-30 03:56:44 +0500

    settings fix

--- it_events/it_events/settings.py
+++ it_events/it_events/settings.py
@@ -119,9 +119,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication'),
+        'rest_framework.authentication.TokenAuthentication',),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny'),
+        'rest_framework.permissions.AllowAny',),
 }
 
 AUTH_USER_MODEL = "users.User"
