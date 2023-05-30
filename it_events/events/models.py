f5470706f3f7e9157934954570956f0e27d2a00a
Artem Pavlishchev <69520287+artemkms@users.noreply.github.com>
2023-05-30 23:58:08 +0700

    Update models.py

--- it_events/events/models.py
+++ it_events/events/models.py
@@ -44,17 +44,14 @@ class Event(models.Model):
 
 
 class City(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name='events', verbose_name='Автор публикации')
     name = models.CharField(
         'Город проведения', max_length=200, unique=True)
 
+    class Meta:
+        ordering = ['name']
+
 
 class Tags(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name='events', verbose_name='Автор публикации')
     name = models.CharField(
         'Тэг', max_length=200)
     slug = models.SlugField(
@@ -70,20 +67,22 @@ class Tags(models.Model):
 
 
 class Format(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name='events', verbose_name='Автор публикации')
     name = models.CharField(
         'Формат', max_length=200, unique=True)
     slug = models.SlugField(
         'Ссылка', max_length=100, unique=True)
 
+    class Meta:
+        verbose_name = 'Формат'
+        verbose_name_plural = 'Форматы'
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.name
+
 
 class Topic(models.Model):
     """Направление(Дизайн, Разработка)"""
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name='events', verbose_name='Автор публикации')
     name = models.CharField(
         'Направление', max_length=200, unique=True)
     slug = models.SlugField(
@@ -92,9 +91,6 @@ class Topic(models.Model):
 
 class SubTopic(models.Model):
     """Под направление(Backend, Frontend)"""
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name='events', verbose_name='Автор публикации')
     name = models.CharField(
         'Направление', max_length=200, unique=True)
     slug = models.SlugField(
