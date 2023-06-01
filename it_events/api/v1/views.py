ffed8278e124888dae70a2ea44e3390588f24f94
Asterrus <gastursolar@gmail.com>
2023-06-01 15:14:48 +0500

    topic viewset

--- it_events/api/v1/views.py
+++ it_events/api/v1/views.py
@@ -2,10 +2,10 @@ from api.v1.filters import EventFilterSet
 from api.v1.paginators import PageLimitPagination
 from api.v1.permissions import IsAdminAuthorOrReadOnly
 from api.v1.serializers import (EventReadSerializer, EventWriteSerializer,
-                                TagSerializer)
+                                TagSerializer, TopicSerializer)
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
-from events.models import Event, Favourite, Tags
+from events.models import Event, Favourite, Tags, Topic
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
@@ -70,3 +70,11 @@ class TagsViewSet(ModelViewSet):
     def get_queryset(self):
         queryset = Tags.objects.annotate(event_count=Count('event'))
         return queryset.order_by('-event_count')
+
+
+class TopicsViewSet(ModelViewSet):
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
+    http_method_names = ['get']
+    filter_backends = [SearchFilter]
+    search_fields = ['name']
