7c03decb559d63b02bfcaf64e2f2ce12772244d0
Asterrus <gastursolar@gmail.com>
2023-06-09 02:52:26 +0500

    static url

--- it_events/it_events/settings.py
+++ it_events/it_events/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
 MEDIA_URL = '/media/'
