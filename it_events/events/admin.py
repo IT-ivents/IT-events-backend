64f56c999dc1250112ab319838e77490b10c1a22
Asterrus <gastursolar@gmail.com>
2023-06-12 19:34:36 +0500

    admin panel change

--- it_events/events/admin.py
+++ it_events/events/admin.py
@@ -1,16 +1,47 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from events.models import City, Event, Format, Tags, Topic
+
+
+@admin.register(Event)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Админ-панель модели Событие
+    """
+    def url_link(self, obj):
+        return format_html(
+            f'<a href="{obj.url}" target="_blank">{obj.url}</a>')
+
+    list_display = ['title', 'author', 'city', 'url_link',
+                    'date_start', 'created_at']
+    list_filter = ['format', 'price', 'date_start', 'created_at']
+    search_fields = ['title', 'organizer', 'city__name',
+                     'topic__name', 'tags__name']
+    raw_id_fields = ['author', 'city', 'topic']
+    date_hierarchy = 'date_start'
+    ordering = ['-created_at']
 
 
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
     """
-    Админ-панель модели Направления
+    Админ-панель модели Направление
     """
     prepopulated_fields = {'slug': ('name',)}
 
+@admin.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
+    """
+    Админ-панель модели Тег
+    """
+    prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Event)
+@admin.register(Format)
+class TagsAdmin(admin.ModelAdmin):
+    """
+    Админ-панель модели Формат
+    """
+    prepopulated_fields = {'slug': ('name',)}
+
 admin.site.register(City)
-admin.site.register(Tags)
-admin.site.register(Format)
+
