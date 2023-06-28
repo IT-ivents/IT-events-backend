80742edc8d63376272110d83792ec6aba863720b
EgorGushin <gea11@yandex.ru>
2023-06-28 14:12:41 +0300

    создание и удаление организации

--- it_events/users/views.py
+++ it_events/users/views.py
@@ -2,8 +2,8 @@ from djoser.views import UserViewSet as DjoserViewSet
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Organisation
-from .permissions import IsManagerOrReeadOnly
+from .models import User, Organisation
+from .permissions import IsManagerOrReadOnly
 from .serializers import OrganisationSerializer
 
 
@@ -12,8 +12,20 @@ class UserViewSet(DjoserViewSet):
 
 
 class OrganisationViewsSet(ModelViewSet):
-    permission_classes = (IsManagerOrReeadOnly,)
+    permission_classes = (IsManagerOrReadOnly,)
     queryset = Organisation.objects.all()
     serializer_class = OrganisationSerializer
     filter_backends = [SearchFilter]
     search_fields = ['name']
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        user.role = User.MANAGER
+        user.save(update_fields=['role'])
+        serializer.save(manager=user)
+
+    def perform_destroy(self, instance):
+        user = self.request.user
+        user.role = User.USER
+        user.save(update_fields=['role'])
+        instance.delete()
