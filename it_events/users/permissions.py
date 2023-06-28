1e824e31ae49e3e0826b4e8e7712631e6923c1d4
EgorGushin <gea11@yandex.ru>
2023-06-28 13:41:33 +0300

    add has_object_permission

--- it_events/users/permissions.py
+++ it_events/users/permissions.py
@@ -13,11 +13,5 @@ class IsManagerOrReadOnly(permissions.BasePermission):
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
             or request.user.is_admin
-        )
-
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-            or request.user.is_admin
+            or request.user.is_manager
         )
