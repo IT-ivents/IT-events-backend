2c0d43954d5d55396e0e12fefbc26614f84d61ff
EgorGushin <gea11@yandex.ru>
2023-06-30 17:59:36 +0300

    добавлено поле Организация у модели юзер

--- it_events/api/v1/urls.py
+++ it_events/api/v1/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from users.views import OrganisationViewsSet, UserViewSet
+from users.views import OrganizationViewsSet, UserViewSet
 
 from .views import (CityViewSet, EventsViewSet, TagsViewSet, TopicsViewSet,
                     cookies_view, privacy_view)
@@ -11,7 +11,7 @@ router.register('events', EventsViewSet, basename='events')
 router.register('tags', TagsViewSet, basename='tags')
 router.register('sities', CityViewSet, basename='sities')
 router.register('topics', TopicsViewSet, basename='topics')
-router.register('organisation', OrganisationViewsSet, basename='organisation')
+router.register('organisation', OrganizationViewsSet, basename='organisation')
 
 app_name = "v1"
 
--- it_events/users/serializers.py
+++ it_events/users/serializers.py
@@ -1,7 +1,7 @@
 from djoser.conf import settings
 from djoser.serializers import UserCreateSerializer as DjoserUsCreateSerializer
 from rest_framework import serializers
-from users.models import Organisation, User
+from users.models import Organization, User
 
 
 class UserCreateSerializer(DjoserUsCreateSerializer):
@@ -9,11 +9,12 @@ class UserCreateSerializer(DjoserUsCreateSerializer):
         model = User
         fields = tuple(User.REQUIRED_FIELDS) + (settings.USER_ID_FIELD,
                                                 'username',
-                                                "password",)
+                                                "password",
+                                                'organization')
 
 
-class OrganisationSerializer(serializers.ModelSerializer):
+class OrganizationSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Organisation
+        model = Organization
         fields = ('manager', 'name')
