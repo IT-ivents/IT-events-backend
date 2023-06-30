01fd10ddb9771dee3b002a826fbb1da5bc065a66
EgorGushin <gea11@yandex.ru>
2023-06-30 17:49:23 +0300

    добавлено поле Организация у модели юзер

--- it_events/users/models.py
+++ it_events/users/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 # from django.core.validators import MinLengthValidator
 from django.db import models
-
 # from events.models import Event
 
 
