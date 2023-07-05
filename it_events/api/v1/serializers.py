ded498ef29c35c4d909700c3fd089b891b806b89
Egor Gushin <gea11@yandex.ru>
2023-07-05 22:44:16 +0300

    Поправлен сериализатор CRUD Event

--- it_events/api/v1/serializers.py
+++ it_events/api/v1/serializers.py
@@ -1,7 +1,7 @@
 from django.db import transaction
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import serializers
 from events.models import City, Event, Format, Tags, Topic
+from rest_framework import serializers
 from users.models import Organisation
 
 
