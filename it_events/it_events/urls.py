d5189a543757da93da8be0d6db769215e38259ea
artemkms <artemkms@gmail.com>
2023-07-18 14:32:45 +0700

    swagger

--- it_events/it_events/urls.py
+++ it_events/it_events/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 # Настройка авто-документации
 schema_view = get_schema_view(openapi.Info(
-    title="Snippets API", default_version='v1',
-    description="Test description",
+    title="Документация 2", default_version='v2.0',
+    description="Автогенерация документации",
     terms_of_service="https://www.google.com/policies/terms/",
     contact=openapi.Contact(email="contact@snippets.local"),
     license=openapi.License(name="BSD License"),
@@ -17,12 +17,9 @@ schema_view = get_schema_view(openapi.Info(
 )
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-            name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-            name='schema-redoc'),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path("api/", include("api.urls", namespace="api")),
 ]
